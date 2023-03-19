e64bd6baa34cbe1d25f3008104d02f74730c0e10
ruxiciortea <ciortearuxandra10@gmail.com>
2023-03-19 17:51:26 +0200

    Created first version of login view

--- Smart Recipes/Controller/LoginViewController.swift
+++ Smart Recipes/Controller/LoginViewController.swift
@@ -7,23 +7,27 @@
 
 import UIKit
 
+let kWelcomeMessage = "Welcome to Smart Recipes!"
+let kSignupMessage = "Don't have an account?"
+
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var appLogoImageView: UIImageView!
+    @IBOutlet weak var loginViewTitle: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signupMessageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupLabels()
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupLabels() {
+        loginViewTitle.text = kWelcomeMessage
+        signupMessageLabel.text = kSignupMessage
     }
-    */
 
 }
